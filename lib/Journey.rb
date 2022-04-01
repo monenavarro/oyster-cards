8f00606a62ee5484c6e5265c762ee0517b748712
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-01 16:21:45 +0100

    finish journey method and tests added

--- lib/Journey.rb
+++ lib/Journey.rb
@@ -1,10 +1,14 @@
 class Journey
 
-  attr_reader :entry_station
+  attr_reader :entry_station, :exit_station
 
   def initialize(entry_station = nil)
     @entry_station = entry_station
     @exit_station = nil
   end
-
+  
+  def finish_journey(station)
+    @exit_station = station
+    @entry_station = nil
+  end
 end
--- spec/Journey_spec.rb
+++ spec/Journey_spec.rb
@@ -2,8 +2,20 @@ require './lib/Journey.rb'
 
 describe Journey do
   let(:station) { double "entry_station"}
+  let(:exit_station) {double "exit station"}
+  
+  
   it "starts a journey" do
   journey = Journey.new(station)
   expect(journey.entry_station).to eq station
+  end
+
+  context '#finish_journey' do
+
+    it {is_expected.to respond_to(:finish_journey)}
+    it 'lets you finish your journey at a station' do
+      subject.finish_journey(exit_station)
+      expect(subject.exit_station).to eq exit_station
+    end
   end
 end
